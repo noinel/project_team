eacaa7fecd1234f6874bafaeb3bdb1e64edf4d16
caasis7920 <46807138+caasis7920@users.noreply.github.com>
2019-01-24 14:03:31 +0900

    Update BLUESENSE.ino

--- BLUESENSE.ino
+++ BLUESENSE.ino
@@ -5,32 +5,51 @@ SoftwareSerial BTSerial(7,6);
 
 int light=0;
 int i;
-
+int light2=1;
+boolean lightToggle = false;
 void setup() 
 {
 BTSerial.begin(9600);
 Serial.begin(9600);
 pinMode(10, INPUT);
 light = digitalRead(10);
+
 }
 
 void loop() 
 {
-  if(light != digitalRead(10))
+  if(true)
   {
-    light = digitalRead(10);
-    if(light == 1)
+    if(light != digitalRead(10))
     {
-      Serial.println(1, DEC);
+        light = digitalRead(10);
+      
+        //Serial.print("L1");
     }
-    else
-    {
-      Serial.println(2, DEC);
+    delay(500);
+    if(light2 != digitalRead(10))
+   {
+        light2 = digitalRead(10);
+        lightToggle = true;
+       // Serial.print("L2");
+    }  
+    if(light == light2 && lightToggle)
+    {  
+      if(light == 1)
+      {
+        Serial.println(2, DEC);
+        lightToggle = false;
+      }
+      else 
+      {
+        Serial.println(1, DEC);
+        lightToggle = false;
+      }
     }
   }
   if(BTSerial.available()){
     int iData = BTSerial.parseInt();
-    if(iData == 1)
+    if(iData == 0)
     {
       Serial.println(3, DEC);
      }
